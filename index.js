6d8fa3e31768955d058452e683df2f41674c87d9
Mimi Cong <mina123fire@yahoo.com>
2020-08-24 21:02:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // write your code below!
-var name = "Susan"
-var height = 24
-isvar message = `${name}  ${height} inches tall`
+var name = "Joe"
+var height = "74"
+var message = '${name}  ${height} inches tall'
+
 console.log(message);
 
 module.exports = {name, height, message}
